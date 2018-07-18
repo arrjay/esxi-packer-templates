90c5aafaed0e42e6cf4a632c0d3df4c9d5f296c7
RJ Bergeron <rbergero@gmail.com>
2018-07-18 01:49:00 +0000

    qemu/uefi build, supply custom nvram block for boot order

--- build-6.7.0.sh
+++ build-6.7.0.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
+ovmf_nvram=$(mktemp)
+ovmf_code_path='ovmf/OVMF_CODE.fd'
+
+cp "ovmf/vars.fd" "${ovmf_nvram}"
+
 packer build \
        -var 'iso_file=iso/VMware-VMvisor-Installer-6.7.0-8169922.x86_64.iso' \
        -var 'iso_checksum_type=sha1' \
        -var 'iso_checksum=332731ffb6715cd7a469ce0f6b81aeda6f624be7' \
+       -var "ovmf_nvram=${ovmf_nvram}" \
+       -var "ovmf_code_path=${ovmf_code_path}" \
        packer-esxi.json
+
+rm "${ovmf_nvram}"
